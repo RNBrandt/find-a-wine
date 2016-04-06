6ca6a6029bebf95ce00afeb5ed1f0d81b253ef98
Reuben Brandt <Reubennbrandt@gmail.com>
2016-04-05 18:26:22 -0700

    Create scraping algorithm
    Author:    Reuben Brandt <Reubennbrandt@gmail.com>
    Date:      Tue Apr 5 18:26:22 2016 -0700

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,28 +1,30 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
 require 'net/http'
 require 'json'
+
 def parse_response(url)
   uri = URI(url)
   response = Net::HTTP.get(uri)
   finished = JSON.parse(response)
-  return @status = finished["Status"]["ReturnCode"]
+  @status = finished["Status"]["ReturnCode"]
+  @products = finished["Products"]
 end
 
 def api_calls
-  multiple = 100
+  multiple = 0
   return_code = 0
-  url = "http://services.wine.com/api/beta2/service.svc/json/catalog?size=#{multiple}&apikey=ad9f081a71cc4ea06097d4c7d4970918"
+  url = "http://services.wine.com/api/beta2/service.svc/json/catalog?size=100&offset=#{multiple}&apikey=ad9f081a71cc4ea06097d4c7d4970918"
   while return_code == 0
     parse_response(url)
     multiple += 100
+    p @products
     p return_code = @status
   end
 end
 
+def create_wine_object(product_array)
+  product_array.each do |product|
+    Wine.new()
+  end
+end
 api_calls
