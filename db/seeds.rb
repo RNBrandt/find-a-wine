43e853a747ba2b92b2634d8da48ca482b4279984
[01;31m[K"name"[m[K: "Reuben Brandt <[01;31m[K"email"[m[K: null>
2016-04-08 11:28:24 -0700

    Add additional algthms

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,24 +7,27 @@ def parse_response(url)
   response = Net::HTTP.get(uri)
   finished = JSON.parse(response)
   @status = finished["Status"]["ReturnCode"]
-  @products = finished["Products"]
+  @products = finished
 end
 
 def api_calls
   multiple = 0
   return_code = 0
-  url = "http://services.wine.com/api/beta2/service.svc/json/catalog?size=100&offset=#{multiple}&apikey=ad9f081a71cc4ea06097d4c7d4970918"
   while return_code == 0
+    url = "http://services.wine.com/api/beta2/service.svc/json/catalog?size=100&offset=#{multiple}&apikey=ad9f081a71cc4ea06097d4c7d4970918"
     parse_response(url)
-    multiple += 100
-    p @products
-    p return_code = @status
+    p multiple += 100
+    @products
+    @products["Products"]["List"][3]["Description"]
+    @return_code = @status
   end
 end
 
-def create_wine_object(product_array)
-  product_array.each do |product|
-    Wine.new()
-  end
-end
+# def create_wine_object(products)
+#   products.each do |product|
+#     p product["List"]
+#     p "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+#   end
+# end
 api_calls
+# create_wine_object(@products)
