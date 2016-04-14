b7cfc6f97dcf87811d3920866d673af6ae851176
Reuben Brandt <Reubennbrandt@gmail.com>
2016-04-14 15:35:08 -0700

    Add all algorithms to get Wine Objects

--- db/migrate/20160406014623_add_columns_to_wines.rb
+++ db/migrate/20160406014623_add_columns_to_wines.rb
@@ -7,6 +7,12 @@ class AddColumnsToWines < ActiveRecord::Migration
     add_column :wines, :year, :string
     add_column :wines, :varietal, :string
     add_column :wines, :vineyard, :string
+    add_column :wines, :vintage, :string
+    add_column :wines, :vintages, :string
+    add_column :wines, :region, :string
+    add_column :wines, :highest_rating, :integer
+    add_column :wines, :description, :text
+    add_column :wines, :description_hash, :text
 
   end
 end
