cf555d515128cf07c581b5442d0a3b9b99c2400e
Reuben Brandt <Reubennbrandt@gmail.com>
2016-04-20 13:43:29 -0700

    Add classes for different API calls

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,64 +2,60 @@
 require 'net/http'
 require 'json'
 
-def parse_response(url)
+class Snooth
+  def initialize(url)
+    @url = url
+  end
+
+  def parse_response
+    uri = URI(url)
+    response = Net::HTTP.get(uri)
+    finished = JSON.parse(response)
+    @products = finished
+  end
+
+  def api_call
+    multiple = 1
+    return_code = 0
+    while return_code != 0
+      url = "http://api.snooth.com/wines/?akey=#{ENV['SNOOTH_API_KEY']}&ip=#{ENV['IP_ADDRESS']}&q=wine&f=#{multiple}&n=100"
+      parse_response(url)
+      p multiple += 100
+      p @products
+      create_wine
+    end
+  end
+end
+
+end
+
+class Wine_com
+  def initialize(url)
+    @url = url
+  end
+
+  def parse_response
   uri = URI(url)
   response = Net::HTTP.get(uri)
   finished = JSON.parse(response)
   @status = finished["Status"]["ReturnCode"]
   @products = finished
-end
-
-def snooth_api_call
-  multiple = 0
-  return_code = 0
-  while return_code == 0
-    url = "http://api.snooth.com/wines/?akey=<#{SNOOTH_API_KEY}>&ip=66.28.234.115&q=napa+cabernet&xp=30"
-    parse_response(url)
-    p multiple += 100
-    @usable_products = @products["Products"]["List"]
-    @return_code = @status
-    create_wine
   end
-end
-end
 
-def wine_com_api_calls
-  multiple = 0
-  return_code = 0
-  while return_code == 0
-    url = "http://services.wine.com/api/beta2/service.svc/json/catalog?size=100&offset=#{multiple}&apikey=#{WINE_COM_API_KEY}"
-    parse_response(url)
-    p multiple += 100
-    @usable_products = @products["Products"]["List"]
-    @return_code = @status
-    create_wine
+  def wine_com_api_calls
+    multiple = 0
+    return_code = 0
+    while return_code == 0
+      url = "http://services.wine.com/api/beta2/service.svc/json/catalog?size=100&offset=#{multiple}&apikey=#{WINE_COM_API_KEY}"
+      parse_response(url)
+      p multiple += 100
+      @usable_products = @products["Products"]["List"]
+      @return_code = @status
+      create_wine_com
+    end
   end
 end
 
-def create_wine
-  @usable_products.each do |child|
-    p "#####################################"
-    # p @type = child["Type"]
-    p @name = child["Name"]["Name"]
-    p @price_min = child["PriceMin"]
-    p @price_max = child["PriceMax"]
-    p @retail = child["PriceRetail"]
-    p @year = child["Year"]
-    p @description = child["Description"]
-    p @highest_rating = child["Ratings"]["HighestScore"]
-    if child["Varietal"]
-      @varietal =  child["Varietal"]["Name"]
-    end
-    if child["Vineyard"]
-      p @vineyard = child["Vineyard"]["Name"]
-    end
-    if child["Appellation"]
-      p @region = child["Appellation"]["Region"]["Name"]
-    end
-  create_wine_object
-  end
-end
 
 def create_wine_object
   wine_var = Wine.new( name: @name, price_min: @price_min, price_max: @price_min, retail: @retail, description: @description, highest_rating: @highest_rating, varietal: @varietal, vineyard: @vineyard, region: @region)
@@ -67,5 +63,5 @@ def create_wine_object
     p wine_var
   end
 end
-api_calls
-# create_wine_object(@products)
+
+snooth_api_call
