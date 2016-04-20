33669e0d6bf75fc9aaa75655fc753d690bd3baf3
[01;31m[K"name"[m[K: "Reuben Brandt <[01;31m[K"email"[m[K: null>
2016-04-20 11:50:07 -0700

    Create place for API keys

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,14 +10,28 @@ def parse_response(url)
   @products = finished
 end
 
-def api_calls
+def snooth_api_call
   multiple = 0
   return_code = 0
   while return_code == 0
-    url = "http://services.wine.com/api/beta2/service.svc/json/catalog?size=100&offset=#{multiple}&apikey=ad9f081a71cc4ea06097d4c7d4970918"
+    url = "http://api.snooth.com/wines/?akey=<#{SNOOTH_API_KEY}>&ip=66.28.234.115&q=napa+cabernet&xp=30"
     parse_response(url)
     p multiple += 100
-    p @usable_products = @products["Products"]["List"]
+    @usable_products = @products["Products"]["List"]
+    @return_code = @status
+    create_wine
+  end
+end
+end
+
+def wine_com_api_calls
+  multiple = 0
+  return_code = 0
+  while return_code == 0
+    url = "http://services.wine.com/api/beta2/service.svc/json/catalog?size=100&offset=#{multiple}&apikey=#{WINE_COM_API_KEY}"
+    parse_response(url)
+    p multiple += 100
+    @usable_products = @products["Products"]["List"]
     @return_code = @status
     create_wine
   end
@@ -27,7 +41,7 @@ def create_wine
   @usable_products.each do |child|
     p "#####################################"
     # p @type = child["Type"]
-    p @name = child["Name"]
+    p @name = child["Name"]["Name"]
     p @price_min = child["PriceMin"]
     p @price_max = child["PriceMax"]
     p @retail = child["PriceRetail"]
@@ -48,7 +62,10 @@ def create_wine
 end
 
 def create_wine_object
-  Wine.create(name: @name, price_min: @price_min, price_max: @price_min, retail: @retail, description: @description, highest_rating: @highest_rating, varietal: @varietal, vineyard: @vineyard, region: @region)
+  wine_var = Wine.new( name: @name, price_min: @price_min, price_max: @price_min, retail: @retail, description: @description, highest_rating: @highest_rating, varietal: @varietal, vineyard: @vineyard, region: @region)
+  if wine_var.save
+    p wine_var
+  end
 end
 api_calls
-
+# create_wine_object(@products)
